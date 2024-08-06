541eeacaf027a11cbbdb0e2cdd5d74aefc999fde
Mayank <mayanksingh122001@gmail.com>
2024-08-06 08:42:53 +0530

    debug

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -92,7 +92,7 @@ module.exports.Login = async (req, res, next) => {
       secure: process.env.NODE_ENV === "production", // Use secure cookies in production
       sameSite: 'strict', // Prevent CSRF attacks
       maxAge: rememberMe ? 30 * 24 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000, // 30 days or 1 day
-      domain: "leetquest.onrender.com"
+      domain: ".leetquest.onrender.com"
     });
 
     res.status(200).json({ 
